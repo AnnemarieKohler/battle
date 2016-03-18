43cd9651a3d949da3a44f0f9d30bcc78974f6d8c
Anne <annemarie.k89@gmail.com>
2016-03-18 17:39:46 +0000

    Current solution for rspec, need to replace Player.new with doubles

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -2,40 +2,24 @@ require 'player'
 require 'game'
 
 describe Game do
-
+  subject(:game) { described_class.new player1, player2}
+  let(:player1) { Player.new 'Player 1' }
+  let(:player2) { Player.new 'Player 2' }
 
   describe '#initialize' do
-    subject(:game) { described_class.new player1, player2}
-
-    let(:player_class) { double :player_class, new: player2}
-    let(:player_class) { double :player_class, new: player1}
-
-    let(:in_turn) { double :in_turn}
-
-    let(:player1) { double :player1 }
-    let(:player2) { double :player2, in_turn: false }
-
-    before(:example) do
-      player2.in_turn = false
-    end
-    it 'initializes with two player names' do
+    it 'initializes with two players' do
       expect(game.players).to include player1, player2
     end
   end
 
   describe '#hugger' do
-
     it 'returns the name of hugger' do
-      pending('Fixing previous test, should resolve this')
       expect(game.hugger).to eq player1
     end
-
-    it 'returns the name of player 2'
   end
 
   describe '#huggee' do
     it 'returns the name of huggee' do
-      pending('Fixing previous test, should resolve this')
       expect(game.huggee).to eq player2
     end
   end
